1e1bd3aec41bb492e9ce52726ce05d964b189512
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-03-07 18:14:27 +0530

    Addeed navbar and panel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import NavBar from './components/navbar';
+import Panel from './components/panels';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* Don't add the codes directly here. Make a component and add it here :) */}
+      <NavBar />
+      <Panel />
     </div>
   );
 }
